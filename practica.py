92ec7aa0ddb426f5ae7f69ab6534ef3f40eace3c
Alejan12 <alejandro.egidojimenez@usp.ceu.es>
2022-10-22 19:55:58 +0200

    subir nuevo

---
+++ practica.py
@@ -0,0 +1,27 @@
+class Vehiculo():
+    def __init__(self, color, ruedas):
+        self.color= color
+        self.ruedas= ruedas
+    def __str__(self):
+        return "color: {} , ruedas: {}".format(self.color, self.ruedas)
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas,velocidad,cilindrada):
+        Vehiculo.__init__(self,color, ruedas)
+        self.velocidad=velocidad
+        self.cilindrada=cilindrada
+    def __str__(self):
+        return Vehiculo.__str__(self)+ ", {} km/h, {} cc".format(self.cilindrada, self.velocidad)
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas,tipo):
+        Vehiculo.__init__(self,color, ruedas)
+        self.tipo=tipo
+    def __str__(self):
+        return Vehiculo.__str__(self)+ ", tipo: {} ".format(self.tipo)
+class Caminoneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada,carga):
+        Coche.__init__(color, ruedas, velocidad, cilindrada)
+        self.carga=carga
+    def __str__(self):
+        return Coche.__str__(self)+", carga: {} kg".format(self.carga)
+c=Caminoneta("azul",4,120,100,80)
+print(c)
